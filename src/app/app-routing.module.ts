ff3d17865c9ac56022e58f642a109e6ce05f2873
Bruna Vieira <bruna.atl@hotmail.com>
2023-10-28 18:50:18 -0300

    Rotas adicionadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { HomeComponent } from './components/home/home.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'produtos', component: ProdutosComponent
+  },
+
+  {
+    path: '', component: HomeComponent
+  },
+
+  {
+    path: 'cadastro', component: CadastroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
