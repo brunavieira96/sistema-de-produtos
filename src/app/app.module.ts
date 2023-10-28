ac2b01162e3da41787fdb70f886ffd795e490b17
Bruna Vieira <bruna.atl@hotmail.com>
2023-10-28 14:35:41 -0300

    Adicionando components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsuariosComponent,
+    ProdutosComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
