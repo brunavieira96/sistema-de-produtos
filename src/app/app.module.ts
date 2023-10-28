3031a3eefad0cd1c38fcb2ed11d9baa8ffe3383c
Bruna Vieira <bruna.atl@hotmail.com>
2023-10-28 14:42:25 -0300

    Corrigindo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { UsuariosComponent } from './components/usuarios/usuarios.component';
 import { ProdutosComponent } from './components/produtos/produtos.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 
@@ -12,7 +11,6 @@ import { CadastroComponent } from './components/cadastro/cadastro.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    UsuariosComponent,
     ProdutosComponent,
     CadastroComponent
   ],
