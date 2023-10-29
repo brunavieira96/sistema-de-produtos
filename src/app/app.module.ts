ad3c019dab2bb3ba41f666f0ec161fe6c0a0c95c
Bruna Vieira <bruna.atl@hotmail.com>
2023-10-29 08:05:07 -0300

    Cadastro de Produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CadastroComponent } from './components/pages/cadastro/cadastro.componen
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { HttpClientModule} from '@angular/common/http'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pages/cadastro/cadastro.component.ts
+++ src/app/components/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Produto } from '../../interfaces/produto';
+import { ProdutosService } from 'src/app/services/produtos.service';
+import Swal from 'sweetalert2';
+
+
 
 @Component({
   selector: 'app-cadastro',
@@ -6,5 +12,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
+  constructor(private produtosService : ProdutosService) {
 
-}
+  }
+
+    produtoForm = new FormGroup({
+    nomeProduto: new FormControl('', Validators.required),
+    preco: new FormControl(),
+    categoria: new FormControl('', Validators.required),
+    quantidade: new FormControl()
+  })
+
+  enviarProduto() {
+    const produto: Produto = this.produtoForm.value as Produto;
+
+    this.produtosService.cadastroProdutos(produto).subscribe(
+      (result) => {
+        Swal.fire({
+        
+          icon: 'success',
+          title: 'O Produto foi Salvo!',
+          showConfirmButton: false,
+          timer: 1500
+      });
+      },
+      (error) => {
+        const { message } = error;
+        Swal.fire('Parece que Houve um Erro no Cadastro', message, 'error');
+      }
+    );
+  }
+
+    
+  }
+
+
+
